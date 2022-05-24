3bd496ff93ffc5992e02f69112f33633ef684203
Luiz Filipe Souza <luiz.filipesouza19@gmail.com>
2022-05-24 18:12:56 -0300

    initial commit

---
+++ costumer/src/main/java/dev/ofilipesouza/costumer/CustomerRegistrationRequest.java
@@ -0,0 +1,4 @@
+package dev.ofilipesouza.costumer;
+
+public record CustomerRegistrationRequest(String firstName, String lastName, String email) {
+}
---
+++ costumer/src/main/java/dev/ofilipesouza/costumer/CustomerRepository.java
@@ -0,0 +1,6 @@
+package dev.ofilipesouza.costumer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+}
